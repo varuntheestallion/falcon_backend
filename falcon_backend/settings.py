45d5567ed9d1ad6f3459180e158d2c36f68e5296
Varun Mathuria <mathuriavarun4566@gmail.com>
2024-05-18 10:03:38 -0700

    Changed from SQLite to PostgreSQL

--- falcon_backend/settings.py
+++ falcon_backend/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = "falcon_backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "falcon_backend",
+        "USER": "varuntheestallion",
+        "PASSWORD": "user",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
